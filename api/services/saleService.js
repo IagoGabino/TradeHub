e5683b5ffbe89527cc7517d528e1a377682cde9b
Matheus Couto <matheus.gcoutoma@gmail.com>
2024-12-14 13:57:26 -0300

    adding sales related files (model, route, service). sales integrate users and products

---
+++ api/services/saleService.js
@@ -0,0 +1,44 @@
+const Sale = require('../models/Sale');
+const Product = require('../models/Product');
+
+const NotFoundError = require('../utils/errors/NotFoundError');
+
+class SaleService {
+    async purchase(body) {
+        const product = await Product.findByPk(body.idLivro);
+        if (product.available) {
+            product.available = false;
+            await product.save();
+            const sale = await Sale.create(body);
+            return sale;
+        } else {
+            throw new NotFoundError('Product not found');
+        }
+    }
+
+    async findAll(id) {
+        const sales = await Sale.findAll({
+            where: {
+                idVendedor: id
+            }
+        });
+        const purchases = await Sale.findAll({
+            where: {
+                idComprador: id
+            }
+        });
+
+        sales.forEach(sale => {
+            sale.dataValues.sale = 1;
+        });
+        purchases.forEach(purchase => {
+            purchase.dataValues.sale = 0;
+        });
+
+        const allSales = sales.concat(purchases);
+
+        return allSales;
+    }
+}
+
+module.exports = new SaleService;
